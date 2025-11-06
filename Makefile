cbc9bdeb2563d9fa3b4170a5be6cf6e39d52179f
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-11-06 09:41:03 +0530

    moved all argument handling to single file

--- Makefile
+++ Makefile
@@ -3,20 +3,17 @@ CFLAGS=-Wall -Wextra -Wpedantic -Wvla
 
 CCOMP=$(CC) $(CFLAGS)
 
-OBJS=ffgeom.o ffioutil.o ff_wraps.o ff_routines.o
+OBJS=ffgeom.o ffioutil.o ff_routines.o
 
 all: ffgeom
 
-ff_routines.o: ff_routines.c ffioutil.h
+ff_routines.o: ff_routines.c ff_routines.h ffioutil.h
 	$(CCOMP) ff_routines.c -c -o ff_routines.o
-
-ff_wraps.o: ff_wraps.c ffioutil.h
-	$(CCOMP) ff_wraps.c -c -o ff_wraps.o
 
 ffioutil.o: ffioutil.c ffioutil.h
 	$(CCOMP) ffioutil.c -c -o ffioutil.o
 
-ffgeom.o: ffgeom.c ffioutil.h
+ffgeom.o: ffgeom.c ff_routines.h ffioutil.h
 	$(CCOMP) ffgeom.c -c -o ffgeom.o
 
 ffgeom: $(OBJS)
--- ffgeom.c
+++ ffgeom.c
@@ -1,11 +1,57 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
+#include <stdio.h>
 
-void ff_wrap_init(int, char*[]);
-void ff_wrap_rect(int, char*[]);
-void ff_wrap_blip(int, char*[]);
-void ff_wrap_ellipse(int argc, char*[]);
+#include <stdint.h>
+#include "ffioutil.h"
+#include "ff_routines.h"
+
+void ff_wrap_init(int argc, char* argv[]) {
+	ff_argchk(argc, 3, "Usage: ff_init width height color\n");
+	uint32_t width = strtol(argv[0], NULL, 0);
+	uint32_t height = strtol(argv[1], NULL, 0);
+	uint8_t clr[8] = FF_COLOR_BASE;
+	ff_getclr_die(argv[2], clr);
+
+	ff_init(width, height, clr);
+}
+
+void ff_wrap_rect(int argc, char* argv[]) {
+	ff_argchk(argc, 5, "Usage: ff_rect x y width height color\n");
+	uint32_t x = strtol(argv[0], NULL, 0);
+	uint32_t y = strtol(argv[1], NULL, 0);
+	uint32_t w = strtol(argv[2], NULL, 0);
+	uint32_t h = strtol(argv[3], NULL, 0);
+	uint8_t clr[8] = FF_COLOR_BASE;
+	ff_getclr_die(argv[4], clr);
+
+	ff_rect(x, y, w, h, clr);
+}
+
+void ff_wrap_blip(int argc, char* argv[]) {
+	ff_argchk(argc, 6, "Usage: ff_blip width height color0 color1 color2 color3\n");
+	uint32_t width = strtol(argv[0], NULL, 0);
+	uint32_t height = strtol(argv[1], NULL, 0);
+	uint8_t clrs[4][8] = { FF_COLOR_BASE, FF_COLOR_BASE, FF_COLOR_BASE, FF_COLOR_BASE };
+	ff_getclr_die(argv[2], clrs[0]);
+	ff_getclr_die(argv[3], clrs[1]);
+	ff_getclr_die(argv[4], clrs[2]);
+	ff_getclr_die(argv[5], clrs[3]);
+
+	ff_blip(width, height, clrs);
+}
+
+void ff_wrap_ellipse(int argc, char* argv[]) {
+	ff_argchk(argc, 5, "Usage: ff_ellipse x y rx ry color\n");
+	uint32_t x = strtol(argv[0], NULL, 0);
+	uint32_t y = strtol(argv[1], NULL, 0);
+	uint32_t rx = strtol(argv[2], NULL, 0);
+	uint32_t ry = strtol(argv[3], NULL, 0);
+	uint8_t clr[8] = FF_COLOR_BASE;
+	ff_getclr_die(argv[4], clr);
+
+	ff_ellipse(x, y, rx, ry, clr);
+}
 
 int main(int argc, char* argv[]) {
 	if(argc < 2) return fprintf(stderr, "Usage: ffgeom [-i file] [operation] [args]\n"), -1;
@@ -21,4 +67,3 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
-
