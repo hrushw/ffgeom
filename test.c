47020640ec036fc1a01e8372103e24c8aadc6ba2
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-11-09 15:06:17 +0530

    added stage to pipeline test

--- test.c
+++ test.c
@@ -27,9 +27,12 @@ end:
 	return out ? out : chldout;
 }
 
-int layer1(int pinfd) {
+int layer1(int pinfd, int poutfd) {
 	close(0);
 	if(fcntl(pinfd, F_DUPFD, 0) != 0) return -1;
+
+	close(1);
+	if(fcntl(poutfd, F_DUPFD, 1) != 1) return -1;
 
 	int out = 0;
 	uint8_t clr[8] = FF_COLOR_BASE;
@@ -39,6 +42,24 @@ int layer1(int pinfd) {
 	}
 	ff_rect(40, 40, 50, 50, clr);
 
+	int chldout = 0;
+end:
+	wait(&chldout);
+	return out;
+}
+
+int layer2(int pinfd) {
+	close(0);
+	if(fcntl(pinfd, F_DUPFD, 0) != 0) return -1;
+
+	int out = 0;
+	uint8_t clr[8] = FF_COLOR_BASE;
+	if(ff_getclr("7f007f", clr)) {
+		out = -1;
+		goto end;
+	}
+	ff_ellipse(120, 120, 20, 40, clr);
+
 end:
 	return out;
 }
@@ -46,19 +67,35 @@ end:
 int main(void) {
 	int iofd1[2];
 	if(pipe(iofd1)) return -1;
+	int iofd2[2];
+	if(pipe(iofd2)) return -1;
 
 	pid_t pid = fork();
 	switch(pid) {
 	case -1:
 		return -1;
-		break;
 
 	case 0:
-		return layer1(iofd1[0]);
+		break;
 
 	default:
 		return layer0(iofd1[1]);
 	}
+	/* Pipeline continues to second layer */
+
+	pid = fork();
+	switch(pid) {
+	case -1:
+		return -1;
+
+	case 0:
+		break;
+
+	default:
+		return layer1(iofd1[0], iofd2[1]);
+	}
+
+	layer2(iofd2[0]);
 
 	return 0;
 }
